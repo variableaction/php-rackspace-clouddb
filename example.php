b39d797f62ac294ab1bc32a59c35f34563995a06
Iain Collins <me@iaincollins.com>
2013-10-16 04:55:56 +0100

    Added methods to grant and revoke user access to a database. Fixed listFlavours(). Add inline documentation. Added example CLI script.
    - Added grantUserAccess() and revokeUserAccess() methods.
    - Added autodoc parseable documentation for all methods.
    - Fixed listFlavours() (wasn't working, guessing the API changed).
    - Added command line script to show example usage for people as lazy as I am.
    - Refactored the param order for the internal method setEndpoints().
    
    Also replaced all tabs with spaces while I was tidying up the formatting.
    I appreciate you may hate this and reject my pull request. Sorry about that.

---
+++ example.php
@@ -0,0 +1,46 @@
+#!/usr/bin/php -q
+<?php
+/**
+ * Simple command line script to show how to use the Rackspace CloudDB class.
+ */
+require_once('rackspace-clouddb.class.php');
+
+const RACKSPACE_USERNAME    = 'YOUR_USERNAME';
+const RACKSPACE_API_KEY     = 'YOUR_API_KEY';
+const RACKSPACE_ACCOUNT_ID  = 'YOUR_ACCOUNT_ID';
+const RACKSPACE_INSTANCE_ID = 'AN_INSTANCE_ID';
+
+$database = 'my-test-database';
+
+// Connect to the Chicago datacenter
+//$rcdb = new RackspaceCloudDB(RACKSPACE_USERNAME,RACKSPACE_API_KEY,RACKSPACE_ACCOUNT_ID,'ORD','US');
+// Connect to the Dallas/Ft. Worth datacenter
+//$rcdb = new RackspaceCloudDB(RACKSPACE_USERNAME,RACKSPACE_API_KEY,RACKSPACE_ACCOUNT_ID,'DFW','US');
+// Connect to the London datacenter
+$rcdb = new RackspaceCloudDB(RACKSPACE_USERNAME,RACKSPACE_API_KEY,RACKSPACE_ACCOUNT_ID,'LON','UK');
+
+// List all instances
+$responseObject = $rcdb->listInstances();
+
+// List all database users in an instance
+//$responseObject = $rcdb->listDatabaseInstanceUsers(RACKSPACE_INSTANCE_ID);
+
+// List all flavours (hardware profiles)
+//$responseObject = $rcdb->listFlavors();
+
+// Create a new database
+//$responseObject = $rcdb->createDatabase(RACKSPACE_INSTANCE_ID, $newDbName);
+
+// Create a new user
+//$responseObject = $rcdb->createUser(RACKSPACE_INSTANCE_ID,$newDbName,'inkrato-app','mkfp28mt');
+
+// Grant a user access to a database
+//$responseObject = $rcdb->grantUserAccess(RACKSPACE_INSTANCE_ID, $newDbName, 'inkrato-app');
+
+// Revoke a users access to a database
+//$responseObject = $rcdb->revokeUserAccess(RACKSPACE_INSTANCE_ID, $newDbName, 'inkrato-app');
+
+// NB: Not all operations (such as creating databases or users) return a response
+echo '<pre>'.print_r($responseObject,true).'</pre>';
+
+?>
